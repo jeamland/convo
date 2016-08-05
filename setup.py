30005f77be2faf2e714db94ab8601a6efe0bf106
Benno Rice <benno@jeamland.net>
2016-08-04 22:19:49 -0700

    More glue.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='convo',
+      version='0.0.1',
+      description='Medium-agnostic conversation wrangler',
+      author='Benno Rice',
+      author_email='benno@jeamland.net',
+      url='https://github.com/jeamland/convo',
+      py_modules=['convo'],
+)
